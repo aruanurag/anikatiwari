852c83d626ebb4602ca083639029ab3be8cd4a16
Anuragg Mohan <anurag.a.mohan@oracle.com>
2026-03-29 15:29:04 -0400

    Configure Astro for www custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
 
-const site = process.env.SITE ?? "https://anuragmohan.github.io";
-const base = process.env.BASE_PATH ?? (process.env.GITHUB_ACTIONS === "true" ? "/anikatiwari" : "/");
-
 export default defineConfig({
-  site,
-  base,
+  site: "https://www.anikatiwari.com",
+  base: "/",
   output: "static"
 });
